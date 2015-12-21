6c2c335ac31a0ff9e55a7420809d8741e9401605
kongfl888 <kongfl888@outlook.com>
2015-12-22 06:17:31 +0800

    userscript: update baiduhttp2https add include.

--- userscript/Baidu_http2https/baiduhttp2https.user.js
+++ userscript/Baidu_http2https/baiduhttp2https.user.js
@@ -4,6 +4,8 @@
 // @description Enforces browsing Baidu Search websites through https
 // @include     http://www.baidu.com/*
 // @include     http://baidu.com/*
+// @include     https://www.baidu.com/*
+// @include     https://baidu.com/*
 // @updateURL   https://github.com/kongfl888/Surfing-the-Internet/raw/master/userscript/Baidu_http2https/baiduhttp2https.meta.js
 // @downloadURL https://github.com/kongfl888/Surfing-the-Internet/raw/master/userscript/Baidu_http2https/baiduhttp2https.user.js
 // @version     1.1
