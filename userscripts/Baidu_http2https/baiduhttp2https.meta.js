82fe753f96ec78f94741dc902599d7ed329c2818
kongfl888 <kongfl888@outlook.com>
2016-07-10 10:02:58 +0800

    userscripts:add "include pan.baidu.com" to Baidu_ http2https

--- userscripts/Baidu_http2https/baiduhttp2https.meta.js
+++ userscripts/Baidu_http2https/baiduhttp2https.meta.js
@@ -4,12 +4,14 @@
 // @description Enforces browsing Baidu Search websites through https
 // @include     http://www.baidu.com/*
 // @include     http://baidu.com/*
+// @include     http://pan.baidu.com/*
+// @include     http://yun.baidu.com/*
 // @include     https://www.baidu.com/*
 // @include     https://baidu.com/*
 // @exclude     http://www.baidu.com/p/setting/*
 // @updateURL   https://github.com/kongfl888/Surfing-the-Internet/raw/master/userscripts/Baidu_http2https/baiduhttp2https.meta.js
 // @downloadURL https://github.com/kongfl888/Surfing-the-Internet/raw/master/userscripts/Baidu_http2https/baiduhttp2https.user.js
-// @version     1.2
+// @version     1.3
 // @author      kongfl888
 // @grant       none
 // ==/UserScript==
